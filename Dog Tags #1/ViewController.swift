484a2ff0b8b7945b5a28b484770cdbf2aec11665
TmanMcNugget <tmccosky1289@stu.d214.org>
2018-10-31 09:44:54 -0500

    Initial Commit

---
+++ Dog Tags #1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dog Tags #1
+//
+//  Created by period2 on 10/31/18.
+//  Copyright © 2018 period2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
