1ef61adfdbb6f06594e671c7c5e8295d8c56756e
TmanMcNugget <tmccosky1289@stu.d214.org>
2018-11-01 09:40:54 -0500

    custom class commit

---
+++ Dog Tags #1/Default_Input_Info.swift
@@ -0,0 +1,14 @@
+//
+//  Default_Input_Info.swift
+//  Dog Tags #1
+//
+//  Created by period2 on 11/1/18.
+//  Copyright © 2018 period2. All rights reserved.
+//
+
+import UIKit
+
+class Default_Input_Info: NSObject
+{
+
+}
