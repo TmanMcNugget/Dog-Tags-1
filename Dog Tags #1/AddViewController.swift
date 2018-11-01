42718bf533107446a66f8a8aea3a19ead35d3267
TmanMcNugget <tmccosky1289@stu.d214.org>
2018-11-01 09:32:00 -0500

    added view controller

---
+++ Dog Tags #1/AddViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddViewController.swift
+//  Dog Tags #1
+//
+//  Created by period2 on 11/1/18.
+//  Copyright © 2018 period2. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
