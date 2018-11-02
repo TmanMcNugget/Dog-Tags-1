9c67c20ff8b9d50a7075660e77acd9257475717c
TmanMcNugget <tmccosky1289@stu.d214.org>
2018-11-02 09:24:49 -0500

    new map

--- Dog Tags #1/ViewController.swift
+++ Dog Tags #1/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 import MapKit
 class ViewController: UIViewController {
     
+    @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
-    
+    var center = (CLLocationCoordinate2D.init(latitude: 38.89731, longitude: -77.00626))
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation])
+    {
+        let myLocation = locations.first!
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        let region = MKCoordinateRegionMake(center, span)
+        self.mapView.setRegion(region, animated: true)
+    }
 
 }
 
