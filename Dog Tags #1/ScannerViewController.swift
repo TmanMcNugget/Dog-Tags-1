44537014a83c46ce302112304941c034ee2ccac9
TmanMcNugget <tmccosky1289@stu.d214.org>
2018-11-09 09:19:11 -0600

    commit 13

--- Dog Tags #1/ScannerViewController.swift
+++ Dog Tags #1/ScannerViewController.swift
@@ -17,28 +17,29 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
     var captureSession:AVCaptureSession?
     var videoPreviewLayer:AVCaptureVideoPreviewLayer?
     var qrCodeFrameView:UIView?
-    var deviceDiscoverySession:Int = 0    
-    let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
-    
-    let captureDevice = deviceDiscoverySession.devices.first
-    else
-    {
-    print("Failed to get the camera device")
-    return
-    }
-    
-    do {
-    let captureMetadataOutput = AVCaptureMetadataOutput()
-    captureSession.addOutput(captureMetadataOutput)
-    let input = try AVCaptureDeviceInput(device: captureDevice)
-    
-    captureSession.addInput(input)
-    
-    } catch {
-    
-    print(error)
-    return
-    }
+//    var deviceDiscoverySession:Int = 0    
+   // let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+//
+   // let captureDevice = deviceDiscoverySession.devices.first
+   // if{}
+//    else
+//    {
+//    print("Failed to get the camera device")
+//    return
+//    }
+//
+//    do {
+//    let captureMetadataOutput = AVCaptureMetadataOutput()
+//    captureSession.addOutput(captureMetadataOutput)
+//    let input = try AVCaptureDeviceInput(device: captureDevice)
+//
+//    captureSession.addInput(input)
+//
+//    } catch {
+//
+//    print(error)
+//    return
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
