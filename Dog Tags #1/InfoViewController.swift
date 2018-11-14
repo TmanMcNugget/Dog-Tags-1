474418a54236c235660b02a4dc7855bb4f815cf9
TmanMcNugget <tmccosky1289@stu.d214.org>
2018-11-14 09:29:17 -0600

    add

--- Dog Tags #1/InfoViewController.swift
+++ Dog Tags #1/InfoViewController.swift
@@ -8,28 +8,22 @@
 
 import UIKit
 
-class InfoViewController: UIViewController {
+class InfoViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var animalTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var petImage: UIImageView!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var extraTextView: UITextView!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
